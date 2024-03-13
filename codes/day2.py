2321affd97adab05657e821d00ce1b6809f54d3b
Senal Dilanjana KAD <138982586+5yndr0m@users.noreply.github.com>
2024-03-13 13:58:49 +0530

    Django day 2

---
+++ codes/day2.py
@@ -0,0 +1,28 @@
+"""
+i = 0
+while i < 10 :
+    print(i)
+    if i == 5 :
+        break
+    i= i + 1
+
+j = 0
+while j < 10 :
+    j = j + 1
+    if j == 5:
+        continue
+    print(j)
+
+for x in "university":
+    print(x)
+
+for x in range(5):
+    print(x)
+
+for x in range(2,9):
+    print(x)
+
+for x in range(1,10,2):
+    print(x)
+
+"""
